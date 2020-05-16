a0ae09dbcf5a67b0dc7d09ff2ceb680679022daf
Joshua Jaramillo <joshamyjarami@hotmail.com>
2020-05-16 15:48:22 -0600

    Added a broken trigger.
    It spins! but it doesnt actually trigger. :c

---
+++ OneStarTaxiInc/Assets/PassengerPickup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassengerPickup : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "PickUp")
+        {
+            Debug.Log("pickup pls");
+            Destroy(this);
+        }
+    }
+}
